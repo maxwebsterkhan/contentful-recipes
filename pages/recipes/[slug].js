fc28aae8f7ae926082e5cb1e9618a35f72587d67
Max Webster-Khan <Max.Webster-Khan@unrvld.com>
2022-09-29 14:37:44 +0100

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -30,7 +30,15 @@ export async function getStaticProps({ params }) {
     content_type: 'recipe',
     'fields.slug': params.slug,
   });
-
+ 
+  if (!items.length) { 
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
   return {
     props: { recipe: items[0] },
     revalidate: 1,
