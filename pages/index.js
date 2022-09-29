d6c814df11379899758af2d7e32fbdfb8638e214
Max Webster-Khan <Max.Webster-Khan@unrvld.com>
2022-09-29 12:51:49 +0100

    moved created client outside get static props

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { createClient } from 'contentful';
 import RecipesCard from '../components/RecipesCard';
 
-export async function getStaticProps() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  });
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
 
+export async function getStaticProps() {
   const res = await client.getEntries({ content_type: 'recipe' });
 
   return {
